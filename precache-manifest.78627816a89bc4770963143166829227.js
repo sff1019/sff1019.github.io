1697eb746db0ca4daccccc81917a06199f0102ea
Hanna Hoshino <starfield1019@gmail.com>
2020-02-26 21:18:54 +0900

    Updates

---
+++ precache-manifest.78627816a89bc4770963143166829227.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421885eee83ae0f6d60d3241de0ac10c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51c1c7fd03829f982683",
+    "url": "/static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "92c08c59c77249d00a88",
+    "url": "/static/js/2.3773470e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.3773470e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c1c7fd03829f982683",
+    "url": "/static/js/main.cd1c751a.chunk.js"
+  },
+  {
+    "revision": "aaed884b9cd5f8f87494",
+    "url": "/static/js/runtime-main.97472ead.js"
+  },
+  {
+    "revision": "6fe14f62e8ec5f37a5b145d8ac89b197",
+    "url": "/static/media/education.6fe14f62.png"
+  },
+  {
+    "revision": "8ec877bedd99415ba5f4313686206143",
+    "url": "/static/media/experience.8ec877be.png"
+  },
+  {
+    "revision": "bf0bb784f84d477b54684b8a4b123128",
+    "url": "/static/media/profile.bf0bb784.png"
+  }
+]);
